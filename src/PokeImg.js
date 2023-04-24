ddd357f75dea7e0b51f9639b2f03353e79042ae3
Bjmarks <bjmarks13@yahoo.com>
2023-04-23 21:56:57 -0500

    flavor text fetch done

--- src/PokeImg.js
+++ src/PokeImg.js
@@ -1,9 +1,18 @@
-import React from 'react'
 export default function pokeImg({ pokemon }) {
-  // console.log(pokemon)
+  const consl = async () => {
+    await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}`).then(res => res.json()).then(data => { 
+      // console.log(data.flavor_text_entries)
+      let x = data.flavor_text_entries;
+      for(let i = 0; i < x.length; i++) {
+        if(x[i].version.name === "red") {
+          console.log(x[i].flavor_text.replaceAll("\n", " ").replaceAll("\f", " "));
+      }
+    }
+  })
+}
   return (
     <div className="image_placeholder">
-        <img id="pokeImg" className="pokeImg" alt="pokemon" title={pokemon.name} src={pokemon.sprites.other.home.front_default || pokemon.sprites.front_default || "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"} />
+        {<img id="pokeImg" className="pokeImg" alt="pokemon" onClick={consl} title={pokemon.name} src={pokemon.sprites.other.home.front_default || pokemon.sprites.front_default || "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"} /> }
     </div>      
   )
 }
--- src/PokeSearch.js
+++ src/PokeSearch.js
@@ -1,19 +1,19 @@
 import React from 'react'
 export default function PokeSearch({ pokemon, pokeS, cpi }) {
-  const handleKeyDown = (event) => {
+  const handleId = (event) => {
     if (event.key === 'Enter' && !isNaN(event.target.value)) {
-      console.log(event.target.value);
       pokeS(event.target.value);
     }
+  }
+  const handleName = (event) => {
     if (event.key === 'Enter') {
-      console.log(event.target.value);
       pokeS(event.target.value);
     }
   }
   return (
     <div className="top-bar">
-        <input type="text" className="name" onKeyDown={handleKeyDown} placeholder={(pokemon.name[0]).toUpperCase()+(pokemon.name).slice(1)} />
-        <input type="text" className="id" onKeyDown={handleKeyDown} placeholder={"#" + pokemon.id.toString().padStart(3, "0")}/>
+        <input type="text" className="name" onKeyDown={handleName} placeholder={(pokemon.name[0]).toUpperCase()+(pokemon.name).slice(1)} />
+        <input type="text" className="id" onKeyDown={handleId} placeholder={"#" + pokemon.id.toString().padStart(3, "0")}/>
     </div>
   )
 }
