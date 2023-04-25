e4c8bc41f3accf5d8fc74fdbee4421a86168f410
Bjmarks <bjmarks13@yahoo.com>
2023-04-25 11:51:50 -0500

    project nearing completion

--- src/PokeImg.js
+++ src/PokeImg.js
@@ -1,18 +1,35 @@
 export default function pokeImg({ pokemon }) {
   const consl = async () => {
+    const descrip = document.getElementById("desc");
+    const img = document.getElementById("pokeImg");
+    img.style.visibility = "hidden";
+    img.style.display = "none";
     await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.id}`).then(res => res.json()).then(data => { 
-      // console.log(data.flavor_text_entries)
       let x = data.flavor_text_entries;
       for(let i = 0; i < x.length; i++) {
-        if(x[i].version.name === "red") {
-          console.log(x[i].flavor_text.replaceAll("\n", " ").replaceAll("\f", " "));
+        if(x[i]?.version.name === "red") {
+          let text = x[i].flavor_text.replaceAll("\n", " ").replaceAll("\f", " ");
+          // console.log(text)
+          descrip.style.display = "flex";
+          descrip.style.visibility = "visible";
+          descrip.innerHTML = text;
       }
     }
   })
 }
+const show = () => {
+  const descrip = document.getElementById("desc");
+  const img = document.getElementById("pokeImg");
+  descrip.style.visibility = "hidden";
+  descrip.style.display = "none";
+  img.style.visibility = "visible";
+  img.style.display = "flex";
+
+}
   return (
     <div className="image_placeholder">
-        {<img id="pokeImg" className="pokeImg" alt="pokemon" onClick={consl} title={pokemon.name} src={pokemon.sprites.other.home.front_default || pokemon.sprites.front_default || "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"} /> }
+      <img id="pokeImg" className="pokeImg" alt="pokemon" onClick={consl} title={pokemon.name} src={pokemon.sprites.other.home.front_default || pokemon.sprites.front_default || "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"} />
+      <p className="description" id="desc" onClick={show}>unable to retrive pokemon desrciption</p>
     </div>      
   )
 }
